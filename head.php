b94a6890e49ada7a738d06f4aa11f11c4028bedc
Pablo <pablomoura2016@gmail.com>
2024-08-04 21:41:57 -0300

    dando inicio ao projeto

---
+++ head.php
@@ -0,0 +1,19 @@
+<?php 
+
+define("BASE_URL", "https://" . $_SERVER['HTTP_HOST'] . "/webcalculator");
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cripto Converter</title>
+    <!-- links bootsteap -->
+    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/bootstrap/bootstrap.min.css">
+    <script src="<?= BASE_URL ?>/assets/bootstrap/bootstrap.bundle.min.js"></script>
+    <!-- links css -->
+    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/style/style.css">
+    <!-- links js -->
+</head>
